060120818a5f94b1d846cbb4f9a2f70574e6f0df
justjacqueline <jacquelinejtay@gmail.com>
2022-01-03 20:49:53 +0900

    Fix roxygen2

--- R/full_join_compare.R
+++ R/full_join_compare.R
@@ -3,30 +3,30 @@
 #' column originated
 #' @param suffix1 A string denoting the suffix to attach to the columns
 #' associated with the first data.frame.  If NA, no suffix attached
-#' @family df join functions
+#' @family join functions
 #' @export
 full_join_compare <- function(df1, df2, by, suffix1, suffix2){
-  
+
   if (is.null(names(by))){ names(by) = by }
-  
+
   join_names <- names(by)
-  
+
   if (!is.na(suffix1)){
     names(df1) <- paste(names(df1), suffix1, sep = "_")
     join_names <- paste(join_names, suffix1, sep = "_")
   }
-  
+
   if (!is.na(suffix2)){
     names(df2) <- paste(names(df2), suffix2, sep = "_")
     by <- paste(by, suffix2, sep = "_")
   }
-  
-  
+
+
   names(by) <- join_names
-  
+
   test <- full_join(df1, df2, by = by) %>%
     select(sort(names(.)))
-  
+
 }
 
 
