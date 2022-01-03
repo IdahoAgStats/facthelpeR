2d11fe1a937c78169ef7a7309fbc1b88eef30e30
justjacqueline <jacquelinejtay@gmail.com>
2022-01-03 09:47:37 +0900

    Copy small-grains-curation/R/qc-cols_fns.R to facthelpeR

---
+++ R/summarize_cols.R
@@ -0,0 +1,175 @@
+#' Find column information
+#' For a data.frame, loop through the columns provided (cols_check) 
+#' and summarise the number of non-NA entries and the groups that 
+#' contain non-NA entries (for example if want to search trial, year, or filename),
+#' and return the first non-NA entry as an example
+#' 
+#' @param cols_check A character vector of column names to check
+#' @param by_col A bare variable to summarise
+find_col_info <- function(df, cols_check, by_col){
+  df2 <- df %>% 
+    mutate(across(.cols = everything(), 
+                  .fns = ~ ifelse(.x == -9, NA, .x))) 
+  
+  ans <- map(cols_check, function(x){
+    x_sym <- sym(x)
+    ans <- df2 %>% 
+      filter(!is.na(!!x_sym)) %>%
+      dplyr::summarise(n = n(),  # count the total number of entries
+                       contained_in = paste(unique({{by_col}}), collapse = ";")) %>% 
+      unique(.) %>% 
+      mutate(variable = x) %>%
+      mutate(example = as.character(first(na.omit(df2[[x]]))))
+  })
+  
+  bind_rows(ans) %>% arrange(variable)
+}
+
+#' Summarize information on variables that match a given regex
+#' 
+#' Summarize information on variables that match a given regex.  
+#' This function loops through a list of data.frames and pulls out the columns 
+#' that match the regex and returns a joined data.frame of the name in the list,
+#' the variable, variable type, and the first instance of the variable as an example.
+#' This function is to help look at raw data in order to check which columns across files
+#' contain the same information.
+#' @param data_list A list of data.frames
+#' @param reg_ex A regular expression to select column names
+#' Use NULL to return all column names
+#' It is possible to edit this function to use the above function (select.colsfromlist)
+summarize.variables <- function(data_list, reg_ex, reg_ex_exclude = NULL){
+  cols_only <- imap(data_list %>% discard(is.null), function(x, y){
+    
+    if (is.null(reg_ex)){
+      dat_cols1 <- x
+    } else {
+      dat_cols1 <- x %>% select(matches(reg_ex))
+    }
+    
+    if (!is.null(reg_ex_exclude)){
+      dat_cols1 <- dat_cols1 %>% select(!matches(reg_ex_exclude))
+    }
+    
+    if (ncol(dat_cols1) == 0){
+      ans2 <- NULL
+    } else {
+      col_type <- dat_cols1 %>%  
+        summarise(across(everything(), class)) %>%
+        slice(1) %>%
+        gather(., colname, type)
+      
+      # Pull an example row of data, pull a row that has the most filled columns
+      col_ex <- dat_cols1 %>% 
+        mutate(count_na = rowSums(is.na(.))) %>%
+        filter(count_na == min(.$count_na)) %>%
+        select(-count_na) %>%
+        slice(1) %>%
+        mutate(across(everything(), as.character)) %>%
+        gather(., colname, example) 
+      
+      ans2 <- left_join(col_type, col_ex, by = "colname") %>% 
+        mutate(file_sheet = y) %>%
+        group_by(file_sheet) %>%
+        mutate(instance = dplyr::row_number()) %>%
+        ungroup()
+    }
+  })
+  
+  temp = bind_rows(cols_only)
+  return(temp)
+  #return(cols_only)
+}
+
+#' Select columns from data.frames stored within a list
+#' 
+#' @param data_list A list with each element containing a data.frame
+#' @param reg_ex A string containing a regular expression that will match the columns to be returned
+#' @param multnames_only A logical expressing whether to only return data 
+#' with more than one column corresponding to a given regular expression
+select.colsfromlist <- function(data_list, reg_ex, multnames_only = FALSE){
+  
+  data_list <- data_list %>% discard(is.null)
+  cols_only <- map(1:length(data_list), function(x){
+    #print(x)
+    dat <- data_list[[x]]
+    name <- names(data_list[x])
+    
+    dat_cols1 <- dat %>% select(matches(reg_ex))
+    
+    if (ncol(dat_cols1) > 0){
+      names(dat_cols1) <- paste(names(dat_cols1), name, x)
+    }
+    
+    dat_cols <- head(dat_cols1)
+    
+    return(dat_cols)
+  })
+  
+  if (multnames_only){
+    cols_only <- cols_only %>% keep(~ncol(.x)>1)
+  }
+  
+  cols_only_bind <- bind_cols(cols_only)
+  return(cols_only_bind)
+  #return(cols_only)
+}
+
+
+#' Summarise the number of columns per data.frame that match a given regex
+#' over a list of data.frames
+#' This function detects columns with similar names within a dataset 
+#'
+#' @param ls A list of data.frames 
+#' @param col_vec A vector containing string regex.  
+#' Each string will correspond to the columns to pull from each dataset
+#' in order to summarize the number of columns associated with that regex string
+summarize.ncol <- function(ls, col_vec){
+  
+  col_vars_n1 <- map(col_vec, function(x){
+    #print(x)
+    cols_summary <- summarize.variables(ls, x)
+    
+    if (nrow(cols_summary) == 0){
+      warning(paste("There are no instances of variable:", x))
+      cols_n <- NULL
+    } else {
+    cols_n <- cols_summary %>% 
+      group_by(file_sheet) %>% 
+      summarise(n = n()) %>% 
+      mutate(col_var = x)
+    }
+    
+  })
+  
+  col_vars_n2 <- bind_rows(col_vars_n1) %>% 
+    pivot_wider(names_from = col_var, values_from = n)
+}
+
+#' Create a list of column names that match a given regex for all data.frames in given list 
+#' The function returns the proportion of the data.frames that contain the column name  
+#' 
+#'
+#' @param ls A list of data.frames 
+#' @param col_vec A vector containing string regex.  
+#' Each string will correspond to the columns to pull from each dataset
+#' in order to summarize the number of columns associated with that regex string
+summarize.colnames <- function(ls, col_vec){
+  
+  col_var_name1 <- map(col_vec, function(x){
+    #print(x)
+    cols_summary <- summarize.variables(ls, x)
+    
+    if (nrow(cols_summary) == 0){
+      warning(paste("There are no instances of variable:", x))
+      col_names <- NULL
+    } else {
+      col_names <- cols_summary %>% 
+        group_by(colname) %>% 
+        summarise(prop_files = n()/length(ls)) %>% 
+        mutate(col_var = x)
+    }
+    
+  })
+  
+  col_var_name2 <- bind_rows(col_var_name1) 
+}
