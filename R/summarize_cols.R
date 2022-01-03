1f72da0732746eb220d5c674e5bba13a4c8d709c
justjacqueline <jacquelinejtay@gmail.com>
2022-01-03 11:18:13 +0900

    Modify summarize columns functions and add wrappers

--- R/summarize_cols.R
+++ R/summarize_cols.R
@@ -1,34 +1,42 @@
-#' Find column information
-#' For a data.frame, loop through the columns provided (cols_check) 
-#' and summarise the number of non-NA entries and the groups that 
+# The jobs of these functions can be clarified and standardized
+# They were written on the fly to perform specific tasks
+# However, their functions can be look at more carefully at a
+# later time
+
+#' Find column information in a data.frame
+#'
+#' For a data.frame, loop through the columns provided (cols_check)
+#' and summarise the number of non-NA entries and the groups that
 #' contain non-NA entries (for example if want to search trial, year, or filename),
 #' and return the first non-NA entry as an example
-#' 
+#'
 #' @param cols_check A character vector of column names to check
 #' @param by_col A bare variable to summarise
+#' @family summarize column functions
+#' @export
 find_col_info <- function(df, cols_check, by_col){
-  df2 <- df %>% 
-    mutate(across(.cols = everything(), 
-                  .fns = ~ ifelse(.x == -9, NA, .x))) 
-  
+  df2 <- df %>%
+    mutate(across(.cols = everything(),
+                  .fns = ~ ifelse(.x == -9, NA, .x)))
+
   ans <- map(cols_check, function(x){
     x_sym <- sym(x)
-    ans <- df2 %>% 
+    ans <- df2 %>%
       filter(!is.na(!!x_sym)) %>%
       dplyr::summarise(n = n(),  # count the total number of entries
-                       contained_in = paste(unique({{by_col}}), collapse = ";")) %>% 
-      unique(.) %>% 
+                       contained_in = paste(unique({{by_col}}), collapse = ";")) %>%
+      unique(.) %>%
       mutate(variable = x) %>%
       mutate(example = as.character(first(na.omit(df2[[x]]))))
   })
-  
+
   bind_rows(ans) %>% arrange(variable)
 }
 
 #' Summarize information on variables that match a given regex
-#' 
-#' Summarize information on variables that match a given regex.  
-#' This function loops through a list of data.frames and pulls out the columns 
+#'
+#' Summarize information on variables that match a given regex.
+#' This function loops through a list of data.frames and pulls out the columns
 #' that match the regex and returns a joined data.frame of the name in the list,
 #' the variable, variable type, and the first instance of the variable as an example.
 #' This function is to help look at raw data in order to check which columns across files
@@ -37,78 +45,82 @@ find_col_info <- function(df, cols_check, by_col){
 #' @param reg_ex A regular expression to select column names
 #' Use NULL to return all column names
 #' It is possible to edit this function to use the above function (select.colsfromlist)
-summarize.variables <- function(data_list, reg_ex, reg_ex_exclude = NULL){
+#' @family summarize column functions
+#' @export
+summarize_variables <- function(data_list, reg_ex, reg_ex_exclude = NULL){
   cols_only <- imap(data_list %>% discard(is.null), function(x, y){
-    
+
     if (is.null(reg_ex)){
       dat_cols1 <- x
     } else {
       dat_cols1 <- x %>% select(matches(reg_ex))
     }
-    
+
     if (!is.null(reg_ex_exclude)){
       dat_cols1 <- dat_cols1 %>% select(!matches(reg_ex_exclude))
     }
-    
+
     if (ncol(dat_cols1) == 0){
       ans2 <- NULL
     } else {
-      col_type <- dat_cols1 %>%  
+      col_type <- dat_cols1 %>%
         summarise(across(everything(), class)) %>%
         slice(1) %>%
         gather(., colname, type)
-      
+
       # Pull an example row of data, pull a row that has the most filled columns
-      col_ex <- dat_cols1 %>% 
+      col_ex <- dat_cols1 %>%
         mutate(count_na = rowSums(is.na(.))) %>%
         filter(count_na == min(.$count_na)) %>%
         select(-count_na) %>%
         slice(1) %>%
         mutate(across(everything(), as.character)) %>%
-        gather(., colname, example) 
-      
-      ans2 <- left_join(col_type, col_ex, by = "colname") %>% 
+        gather(., colname, example)
+
+      ans2 <- left_join(col_type, col_ex, by = "colname") %>%
         mutate(file_sheet = y) %>%
         group_by(file_sheet) %>%
         mutate(instance = dplyr::row_number()) %>%
         ungroup()
     }
   })
-  
-  temp = bind_rows(cols_only)
+
+  temp <- bind_rows(cols_only)
   return(temp)
   #return(cols_only)
 }
 
 #' Select columns from data.frames stored within a list
-#' 
+#'
 #' @param data_list A list with each element containing a data.frame
 #' @param reg_ex A string containing a regular expression that will match the columns to be returned
-#' @param multnames_only A logical expressing whether to only return data 
+#' @param multnames_only A logical expressing whether to only return data
 #' with more than one column corresponding to a given regular expression
-select.colsfromlist <- function(data_list, reg_ex, multnames_only = FALSE){
-  
+#' @family summarize column functions
+#' @export
+select_colsfromlist <- function(data_list, reg_ex, multnames_only = FALSE){
+
   data_list <- data_list %>% discard(is.null)
   cols_only <- map(1:length(data_list), function(x){
     #print(x)
     dat <- data_list[[x]]
     name <- names(data_list[x])
-    
+
     dat_cols1 <- dat %>% select(matches(reg_ex))
-    
+
     if (ncol(dat_cols1) > 0){
       names(dat_cols1) <- paste(names(dat_cols1), name, x)
     }
-    
+
     dat_cols <- head(dat_cols1)
-    
+
     return(dat_cols)
   })
-  
+
   if (multnames_only){
     cols_only <- cols_only %>% keep(~ncol(.x)>1)
   }
-  
+
   cols_only_bind <- bind_cols(cols_only)
   return(cols_only_bind)
   #return(cols_only)
@@ -117,59 +129,113 @@ select.colsfromlist <- function(data_list, reg_ex, multnames_only = FALSE){
 
 #' Summarise the number of columns per data.frame that match a given regex
 #' over a list of data.frames
-#' This function detects columns with similar names within a dataset 
+#' This function detects columns with similar names within a dataset
 #'
-#' @param ls A list of data.frames 
-#' @param col_vec A vector containing string regex.  
+#' @param ls A list of data.frames
+#' @param col_vec A vector containing string regex.
 #' Each string will correspond to the columns to pull from each dataset
 #' in order to summarize the number of columns associated with that regex string
-summarize.ncol <- function(ls, col_vec){
-  
+#' @family summarize column functions
+#' @export
+summarize_ncol <- function(ls, col_vec){
+
   col_vars_n1 <- map(col_vec, function(x){
     #print(x)
     cols_summary <- summarize.variables(ls, x)
-    
+
     if (nrow(cols_summary) == 0){
       warning(paste("There are no instances of variable:", x))
       cols_n <- NULL
     } else {
-    cols_n <- cols_summary %>% 
-      group_by(file_sheet) %>% 
-      summarise(n = n()) %>% 
+    cols_n <- cols_summary %>%
+      group_by(file_sheet) %>%
+      summarise(n = n()) %>%
       mutate(col_var = x)
     }
-    
+
   })
-  
-  col_vars_n2 <- bind_rows(col_vars_n1) %>% 
+
+  col_vars_n2 <- bind_rows(col_vars_n1) %>%
     pivot_wider(names_from = col_var, values_from = n)
 }
 
-#' Create a list of column names that match a given regex for all data.frames in given list 
-#' The function returns the proportion of the data.frames that contain the column name  
-#' 
+#' Create a list of column names that match a given regex for all data.frames in given list
+#' The function returns the proportion of the data.frames that contain the column name
 #'
-#' @param ls A list of data.frames 
-#' @param col_vec A vector containing string regex.  
+#'
+#' @param ls A list of data.frames
+#' @param col_vec A vector containing string regex.
 #' Each string will correspond to the columns to pull from each dataset
 #' in order to summarize the number of columns associated with that regex string
-summarize.colnames <- function(ls, col_vec){
-  
+#' @family summarize column functions
+#' @export
+summarize_colnames <- function(ls, col_vec){
+
   col_var_name1 <- map(col_vec, function(x){
     #print(x)
     cols_summary <- summarize.variables(ls, x)
-    
+
     if (nrow(cols_summary) == 0){
       warning(paste("There are no instances of variable:", x))
       col_names <- NULL
     } else {
-      col_names <- cols_summary %>% 
-        group_by(colname) %>% 
-        summarise(prop_files = n()/length(ls)) %>% 
+      col_names <- cols_summary %>%
+        group_by(colname) %>%
+        summarise(prop_files = n()/length(ls)) %>%
         mutate(col_var = x)
     }
-    
+
   })
-  
-  col_var_name2 <- bind_rows(col_var_name1) 
+
+  col_var_name2 <- bind_rows(col_var_name1)
+}
+
+#' A wrapper for summarize_variables to maintain backwards compatibility
+#'
+#' @rdname summarize_variables
+#' @export summarize.variables
+summarize.variables <- function(data_list, reg_ex, reg_ex_exclude = NULL){
+  message("This function name is being retained for backwards compatibility.
+          Please use summarize_variables()")
+
+  summarize_variables(data_list = data_list,
+                      reg_ex = reg_ex,
+                      reg_ex_exclude = reg_ex_exclude)
+}
+
+
+#' A wrapper for select_colsfromlist to maintain backwards compatibility
+#'
+#' @rdname select_colsfromlist
+#' @export select.colsfromlist
+select.colsfromlist <- function(data_list, reg_ex, multnames_only = FALSE){
+  message("This function name is being retained for backwards compatibility.
+          Please use select_colsfromlist()")
+
+  select_colsfromlist(data_list = data_list,
+                      reg_ex = reg_ex,
+                      multnames_only = multnames_only)
+}
+
+
+#' A wrapper for summarize_ncol to maintain backwards compatibility
+#'
+#' @rdname summarize_ncol
+#' @export summarize.ncol
+summarize.ncol <- function(ls, col_vec){
+  message("This function name is being retained for backwards compatibility.
+          Please use summarize_ncol()")
+
+  summarize_ncol(ls = ls, col_vec = col_vec)
+}
+
+#' A wrapper for summarize_colnames to maintain backwards compatibility
+#'
+#' @rdname summarize_colnames
+#' @export summarize.colnames
+summarize.colnames <- function(ls, col_vec){
+  message("This function name is being retained for backwards compatibility.
+          Please use summarize_colnames()")
+
+  summarize_colnames(ls = ls, col_vec = col_vec)
 }
--- tests/testthat/test-summarize_cols.R
+++ tests/testthat/test-summarize_cols.R
@@ -1,14 +1,12 @@
-source(here::here("R", "dependencies.R"))
-
 test_that("summarize.variables returns the correct summary", {
   df1 <- data.frame(y0 = c(1, 2, 3), y2 = c(4, 5, 6))
   df2 <- data.frame(y1 = c(3, 2, 1), y2 = c(6, 5, 4))
   ls1 <- list(df1, df2)
   names(ls1) <- c("df1", "df2")
-  ans <- tibble(colname = c("y0", "y2", "y1", "y2"), 
-                type = rep("numeric", 4), 
-                example = c("1","4","3","6"), 
-                file_sheet = c(rep("df1",2), 
+  ans <- tibble(colname = c("y0", "y2", "y1", "y2"),
+                type = rep("numeric", 4),
+                example = c("1","4","3","6"),
+                file_sheet = c(rep("df1",2),
                                rep("df2", 2)),
                 instance = rep(c(1L,2L), 2))
   expect_equal(summarize.variables(ls1, NULL), ans)
@@ -20,5 +18,5 @@ test_that("find_col_info()", {
   ans <- data.frame(n = c(3, 1, 2), contained_in = c("91;92", "92", "92"), variable = cols, example = c("1", "2", "a"))
   test <- find_col_info(df, cols, year)
   expect_equal(test,ans)
-  
+
 })
