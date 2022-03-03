90f820c2b4c033e3bcad7728ddfd9d5230be5c9d
emilymg <emilymgalvin@gmail.com>
2022-03-03 10:09:22 -0500

    update read_ods_multsheets and build site

--- R/read_ods_multsheets.R
+++ R/read_ods_multsheets.R
@@ -4,19 +4,6 @@
 #'
 #' @return A list of data frames--one data frame for each sheet
 #' @export
-#'
-#' @examples
-#' dat <- c("iris", "mtcars")
-#' path <- "/home/em/Desktop/"
-#'
-#' readODS::write_ods(get(dat[1]), paste(path, "x.ods"), row_names = FALSE, sheet = dat[1])
-#'
-#' for(i in 2:length(dat)) {
-#' readODS::write_ods(get(dat[i]), paste(path, "x.ods"), row_names = FALSE, sheet = dat[i],
-#'   append = TRUE)}
-#'
-#' read_ods_multsheets(paste(path, "x.ods"))
-#'
 read_ods_multsheets <- function(filename) {
   sheets <- readODS::list_ods_sheets(filename)
   x <- lapply(sheets, function(X) readODS::read_ods(filename, sheet = X))
