9fad6e69c3978cb821d31b62d3d327a0acbe6476
emilymg <emilymgalvin@gmail.com>
2022-03-08 10:01:02 -0500

    add read_ods_multsheets to family readin functions

--- R/read_ods_multsheets.R
+++ R/read_ods_multsheets.R
@@ -3,6 +3,7 @@
 #' @param filename File to import
 #'
 #' @return A list of data frames--one data frame for each sheet
+#' @family readin functions
 #' @export
 read_ods_multsheets <- function(filename) {
   sheets <- readODS::list_ods_sheets(filename)
