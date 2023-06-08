e7607cd0d06218709ad7619f1988ed032837c6ab
emilymg <emilymgalvin@gmail.com>
2023-06-08 16:24:06 -0400

    fix R CMD CHECK warnings and build_site

--- tests/testthat/test-full_join_compare.R
+++ tests/testthat/test-full_join_compare.R
@@ -4,7 +4,7 @@ test_that("full_join_compare() returns expected join", {
   df2 <- data.frame(a = c(1,3), c = c("y", "z"))
   test <- full_join_compare(df1, df2, by = "a", "df1", "df2")
   ans <- data.frame(a_df1 = c(1,2,3), b_df1 = c("a", "b", NA), c_df2 = c("y", NA, "z"))
-  
+
   expect_equal(test,ans)
 })
 
@@ -13,7 +13,7 @@ test_that("full_join_compare() returns expected join", {
   df2 <- data.frame(a = c(1,3), c = c("y", "z"))
   test <- full_join_compare(df1, df2, by = "a", NA, "df2")
   ans <- data.frame(a = c(1,2,3), b = c("a", "b", NA), c_df2 = c("y", NA, "z"))
-  
+
   expect_equal(test,ans)
 })
 
@@ -23,7 +23,7 @@ test_that("full_join_compare() returns expected join", {
   df2 <- data.frame(a = c(1,3), c = c("y", "z"))
   test <- full_join_compare(df1, df2, by = "a", "df1", NA)
   ans <- data.frame(a_df1 = c(1,2,3), b_df1 = c("a", "b", NA), c = c("y", NA, "z"))
-  
+
   expect_equal(test,ans)
 })
 
@@ -32,7 +32,7 @@ test_that("full_join_compare() returns expected join", {
   df2 <- data.frame(a = c(1,3), c = c("y", "z"))
   test <- full_join_compare(df1, df2, by = "a", NA, NA)
   ans <- data.frame(a = c(1,2,3), b = c("a", "b", NA), c = c("y", NA, "z"))
-  
+
   expect_equal(test,ans)
 })
 
@@ -41,7 +41,7 @@ test_that("full_join_compare() returns expected join using 2 join variables", {
   df2 <- data.frame(a = c(1,3), b = c("a", "a") , d = c("y", "z"))
   test <- full_join_compare(df1, df2, by = c("a", "b"), "df1", "df2")
   ans <- data.frame(a_df1 = c(1,2,3), b_df1 = c("a", "b", "a"), c_df1 = c("y", "z", NA), d_df2 = c("y", NA, "z"))
-  
+
   expect_equal(test,ans)
 })
 
